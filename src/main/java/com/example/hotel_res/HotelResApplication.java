d4fa8407071e1494d058d6a59c09811f4480cbea
kawshalj99 <163717566+kawshalj99@users.noreply.github.com>
2024-09-05 08:05:49 +0530

    Backend

---
+++ src/main/java/com/example/hotel_res/HotelResApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotel_res;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelResApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelResApplication.class, args);
+	}
+
+}
